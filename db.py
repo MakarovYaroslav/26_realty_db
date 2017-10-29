a15d11ac1ccf91438b60ff1c2533a91180915772
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-10-29 22:25:33 +0300

    Добавлено описание модели БД

---
+++ db.py
@@ -0,0 +1,36 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Ads(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    settlement = db.Column(db.String(40))
+    under_construction = db.Column(db.Boolean)
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    oblast_district = db.Column(db.String(40))
+    living_area = db.Column(db.Float)
+    has_balcony = db.Column(db.Boolean)
+    address = db.Column(db.String(40))
+    construction_year = db.Column(db.Integer)
+    rooms_number = db.Column(db.Integer)
+    premise_area = db.Column(db.Float)
+    active = db.Column(db.Boolean)
+
+    def __init__(self, id, settlement, under_construction, description, price,
+                 oblast_district, living_area, has_balcony, address,
+                 construction_year, rooms_number, premise_area, active):
+        self.id = id
+        self.settlement = settlement
+        self.under_construction = under_construction
+        self.description = description
+        self.price = price
+        self.oblast_district = oblast_district
+        self.living_area = living_area
+        self.has_balcony = has_balcony
+        self.address = address
+        self.construction_year = construction_year
+        self.rooms_number = rooms_number
+        self.premise_area = premise_area
+        self.active = active
