e2111013a659a17b792bb2b1c96acedc893849c2
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-11-12 19:58:51 +0300

    убраны повторения в проверке условий

--- server.py
+++ server.py
@@ -24,32 +24,14 @@ def ads_list():
     max_price_of_ads = Ads.query.order_by(Ads.price)[-1].price
     max_price = request.args.get('max_price', max_price_of_ads)
     current_year = datetime.today().year
-    if oblast_district is not None and bool(new_building) is True:
-        ads = Ads.query.filter(Ads.active == True,
-                               and_(Ads.price <= max_price,
-                                    Ads.price >= min_price),
-                               Ads.oblast_district == oblast_district,
-                               or_(Ads.under_construction == True,
-                                   Ads.construction_year >= current_year-2)
-                               ).paginate(int(page), ads_per_page, False)
-    elif oblast_district is None and bool(new_building) is True:
-        ads = Ads.query.filter(Ads.active == True,
-                               and_(Ads.price <= max_price,
-                                    Ads.price >= min_price),
-                               or_(Ads.under_construction == True,
-                                   Ads.construction_year >= current_year - 2)
-                               ).paginate(int(page), ads_per_page, False)
-    elif oblast_district is not None and bool(new_building) is False:
-        ads = Ads.query.filter(Ads.active == True,
-                               and_(Ads.price <= max_price,
-                                    Ads.price >= min_price),
-                               Ads.oblast_district == oblast_district
-                               ).paginate(int(page), ads_per_page, False)
-    else:
-        ads = Ads.query.filter(Ads.active == True,
-                               and_(Ads.price <= max_price,
-                                    Ads.price >= min_price)
-                               ).paginate(int(page), ads_per_page, False)
+    ads = Ads.query.filter(Ads.active == True)
+    ads = ads.filter(and_(Ads.price <= max_price, Ads.price >= min_price))
+    if oblast_district is not None:
+        ads = ads.filter(Ads.oblast_district == oblast_district)
+    if bool(new_building) is True:
+        ads = ads.filter(or_(Ads.under_construction == True,
+                             Ads.construction_year >= current_year-2))
+    ads = ads.paginate(int(page), ads_per_page, False)
     pagination = Pagination(page=page, total=ads.total,
                             per_page=ads_per_page, css_framework='bootstrap3')
     return render_template('ads_list.html', ads=ads,
