d9ed1a9cac361a43433938abf9d881520d16a79d
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-11-12 20:17:50 +0300

    Добавлена обработка неверных данных при добавлении

--- update_db.py
+++ update_db.py
@@ -6,28 +6,31 @@ from server import app
 def insert_ads_to_db(json_data):
     for ad in json_data:
         existing_ad = Ads.query.get(ad['id'])
-        if existing_ad is None:
-            ad_in_db = Ads(ad['id'], ad['settlement'],
-                           ad['under_construction'],
-                           ad['description'], ad['price'],
-                           ad['oblast_district'], ad['living_area'],
-                           ad['has_balcony'], ad['address'],
-                           ad['construction_year'], ad['rooms_number'],
-                           ad['premise_area'], active=True)
-            db.session.add(ad_in_db)
-        else:
-            existing_ad.settlement = ad['settlement']
-            existing_ad.under_construction = ad['under_construction']
-            existing_ad.description = ad['description']
-            existing_ad.price = ad['price']
-            existing_ad.oblast_district = ad['oblast_district']
-            existing_ad.living_area = ad['living_area']
-            existing_ad.has_balcony = ad['has_balcony']
-            existing_ad.address = ad['address']
-            existing_ad.construction_year = ad['construction_year']
-            existing_ad.rooms_number = ad['rooms_number']
-            existing_ad.premise_area = ad['premise_area']
-            existing_ad.active = True
+        try:
+            if existing_ad is None:
+                ad_in_db = Ads(ad['id'], ad['settlement'],
+                               ad['under_construction'],
+                               ad['description'], ad['price'],
+                               ad['oblast_district'], ad['living_area'],
+                               ad['has_balcony'], ad['address'],
+                               ad['construction_year'], ad['rooms_number'],
+                               ad['premise_area'], active=True)
+                db.session.add(ad_in_db)
+            else:
+                existing_ad.settlement = ad['settlement']
+                existing_ad.under_construction = ad['under_construction']
+                existing_ad.description = ad['description']
+                existing_ad.price = ad['price']
+                existing_ad.oblast_district = ad['oblast_district']
+                existing_ad.living_area = ad['living_area']
+                existing_ad.has_balcony = ad['has_balcony']
+                existing_ad.address = ad['address']
+                existing_ad.construction_year = ad['construction_year']
+                existing_ad.rooms_number = ad['rooms_number']
+                existing_ad.premise_area = ad['premise_area']
+                existing_ad.active = True
+        except KeyError:
+            pass
     db.session.commit()
     return
 
